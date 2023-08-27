1329ccdd6b2497e00ae1653160d8c9741d3c4f15
tre3 <goddywin@gmail.com>
2023-08-27 20:41:13 +0800

    incomplete result checker

--- Mastermindjs.js
+++ Mastermindjs.js
@@ -30,14 +30,15 @@ const generateSecret = () => {
     secret.push(selectedColor);
     colorCount[randomInt].count++;
   }
-  console.log(secret);
+  // console.log(secret);
   return secret;
 };
+generateSecret();
 
 /*----- functions -----*/
 function changeButtonColor() {
   const pChoice = document.querySelectorAll(".playerChoice");
-  const checkers = document.querySelectorAll(".checker");
+  const checkers = document.querySelectorAll(".checking");
 
   let clickedvalue = "";
 
@@ -53,6 +54,7 @@ function changeButtonColor() {
   for (let i = 0; i < checkers.length; i++) {
     checkers[i].addEventListener("click", function changevalue() {
       checkers[i].style.backgroundColor = clickedvalue;
+      checkers[i].value = clickedvalue;
     });
   }
 }
@@ -79,3 +81,58 @@ function init() {
 }
 
 init();
+
+//breakdown when submit what to check for
+
+//1. put answer into an array
+//2. check color of a1 vs secretcode if !== to all 4, produce white
+//3. check color of a1 if, absolute correct, produce red
+//4. check color of a1, if color is correct, position wrong, produce white
+
+// to note, a2 needs to check a1's color, a3 needs to check a1,a2, a4 needs to check a1,a2,a3
+
+//* when click submit, change class to "checked" and "checked" will disable clicking
+//* after click submit, the row above will be clickable
+//* row above will generate submit button
+
+//! will add on submit proc later, dishing out the inner codes first
+const checking = document.querySelectorAll(".checking");
+// console.log(checking[0].id);
+
+const submitArray = [];
+
+// generate ID, assign 0,1,2,3 to buttons and push to submitArray
+for (i = 0; i < checking.length; i++) {
+  checking[i].setAttribute("id", i);
+
+  const id = checking[i].id;
+  const value = checking[i].getAttribute("value");
+
+  submitArray.push({ id, value });
+
+  console.log(secret);
+
+  // results for absolutely correct
+  if (
+    secret[i] === checking[i].value &&
+    secret.indexOf(secret[i]) === parseInt(checking[i].id)
+  ) {
+    console.log("show red peg");
+  } else if (secret.includes(value) && secret.indexOf(value) !== i) {
+    console.log("Show white peg");
+    // now check other spaces whether color is existent, if yes show white peg first (can edit bug later)
+  } else {
+    console.log("dont show peg");
+  }
+}
+console.log(submitArray);
+
+// console.log(submitArray[0].value);
+// console.log(secret);
+
+// if (submitArray[0].value === secret[0]) {
+//   console.log("show red peg");
+// }
+
+// create for loop, everytime it is pushed into submitArray, will check against secret
+// if submitArray[0] !== true, check against submitArray[i] i--
